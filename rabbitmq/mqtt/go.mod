df31826af10715ff832a5b3a4aed6db8300c53a4
bob bae <bob.bae@gmail.com>
2022-04-13 10:16:36 -0700

    mqtt

--- rabbitmq/mqtt/go.mod
+++ rabbitmq/mqtt/go.mod
@@ -3,11 +3,13 @@ module mqttdemo
 go 1.18
 
 require (
-	github.com/akamensky/argparse v1.3.1
 	github.com/eclipse/paho.mqtt.golang v1.3.5
+	github.com/tjarratt/babble v0.0.0-20210505082055-cbca2a4833c1
 )
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
-	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.19.0 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 )
--- rabbitmq/mqtt/main.go
+++ rabbitmq/mqtt/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
 	"log"
+	"flag"
 	"net/url"
-	"os"
+	//"os"
 	"strings"
 	"time"
 
-	"github.com/akamensky/argparse"
+	"github.com/tjarratt/babble"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -21,23 +22,14 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func parseUserArgs() (string, string) {
-	parser := argparse.NewParser("Chat", "Chat using AMQP and Go")
-
-	// Create flags for username and password
-	nameOptions := argparse.Options{Required: true, Help: "User name"}
-	name := parser.String("u", "user", &nameOptions)
-	passwordOptions := argparse.Options{Required: true, Help: "Password"}
-	password := parser.String("p", "password", &passwordOptions)
-
-	// Parse input
-	err := parser.Parse(os.Args)
-	failOnError(err, "Error while parsing arguments")
-	return *name, *password
-}
-
 func connect(uri *url.URL) mqtt.Client {
-	opts := createClientOptions(uri)
+	password, _ := uri.User.Password()
+	name := uri.User.Username()
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s", uri.Host))
+	opts.SetUsername(name)
+	opts.SetPassword(password)
+
 	client := mqtt.NewClient(opts)
 	token := client.Connect()
 	for !token.WaitTimeout(time.Microsecond) {
@@ -46,38 +38,8 @@ func connect(uri *url.URL) mqtt.Client {
 	return client
 }
 
-func createClientOptions(uri *url.URL) *mqtt.ClientOptions {
-	password, _ := uri.User.Password()
-	name := uri.User.Username()
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("tcp://%s", uri.Host))
-	opts.SetUsername(name)
-	opts.SetPassword(password)
-	return opts
-}
-
 func showMessage(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("* %s\n", string(msg.Payload()))
-}
-
-func listen(uri *url.URL) {
-	client := connect(uri)
-	client.Subscribe(parseTopic(uri), QOS_AT_MOST_ONCE, showMessage)
-}
-
-func sendMessage(msg string, uri *url.URL) {
-	client := connect(uri)
-	RETAIN_MESSAGE := false
-	client.Publish(parseTopic(uri), QOS_AT_MOST_ONCE, RETAIN_MESSAGE, msg)
-}
-
-func poolMessage(uri *url.URL, user string) {
-	for {
-		r := bufio.NewReader(os.Stdin)
-		msg, _ := r.ReadString('\n')
-		msg = fmt.Sprintf("%s: %s", user, strings.TrimSpace(msg))
-		sendMessage(msg, uri)
-	}
 }
 
 func parseTopic(uri *url.URL) string {
@@ -89,14 +51,30 @@ func parseTopic(uri *url.URL) string {
 }
 
 func main() {
-	user, passwd := parseUserArgs()
-	fullUrl := fmt.Sprintf("mqtt://%s:%s@localhost:1883/test", user, passwd)
-	uri, err := url.Parse(fullUrl)
+	user := flag.String("u", "user", "user name")
+	password := flag.String("p", "password", "password")
+	flag.Parse()
+	mqttURL := fmt.Sprintf("mqtt://%s:%s@localhost:1883/test", *user, *password)
+	uri, err := url.Parse(mqttURL)
 	failOnError(err, "Failed to parse given URL")
 
-	forever := make(chan bool)
-	go listen(uri)
-	go poolMessage(uri, user)
-	<-forever
+	go func(uri *url.URL) {
+		client := connect(uri)
+		client.Subscribe(parseTopic(uri), QOS_AT_MOST_ONCE, showMessage)
+	}(uri)
+
+	client := connect(uri)
+	RETAIN_MESSAGE := false
+
+	babbler := babble.NewBabbler()
+	babbler.Separator = " "
+
+	for {
+		//r := bufio.NewReader(os.Stdin)
+		//msg, _ := r.ReadString('\n')
+		msg := fmt.Sprintf("%s: %s", *user, strings.TrimSpace(babbler.Babble()))
+		client.Publish(parseTopic(uri), QOS_AT_MOST_ONCE, RETAIN_MESSAGE, msg)
+		time.Sleep(time.Second * 1)
+	}
 }
 
